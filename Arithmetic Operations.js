c4eebc5633b405cebf9195c4f878ce7ee6b5ce61
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2023-01-23 20:41:13 +0530

    Create Arithmetic Operations.js

---
+++ Arithmetic Operations.js
@@ -0,0 +1,22 @@
+var  exports = module.exports={}
+exports.AddNumber=function(a,b){   
+   c=a+b;
+   console.log("The Addition Of",a,"and", b, "is" ,c);
+   
+};
+exports.SubstractNumber=function(a,b){
+    c=a-b;
+    console.log("The Substraction Of",a,"and", b, "is" ,c);
+   
+};
+exports.MultiplyNumber=function(a,b){
+    c=a*b;
+    console.log("The Multiplication Of",a,"and", b, "is" ,c);
+   
+};
+exports.DivideNumbers=function(a,b){
+    c=a/b;
+    console.log("The Division Of",a,"and", b, "is" ,c);
+    
+}
+console.log("Enjoy Coding !!!");
